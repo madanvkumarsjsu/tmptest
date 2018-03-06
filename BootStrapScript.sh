96aaa32d93d21efa0a4dd0a8893c22ca35cffdbb
Madan Kumar <madankumar.vijayakumar@sjsu.edu>
2018-03-06 14:06:58 -0800

    Script Commit1

--- BootStrapScript.sh
+++ BootStrapScript.sh
@@ -3,9 +3,9 @@
 osUserName=$1
 echo $osUserName >> /tmp/username.log
 echo "Success" >> /tmp/username.log
-# testDir=/home/$osUserName/testmadan
-# mkdir $testDir
+testDir=/home/$osUserName/testmadan
+mkdir $testDir
 
-# cd $testDir
-# wget -S -T 10 -t 5 http://elasticmapreduce.s3.amazonaws.com/bootstrap-actions/file.tar.gz
-# tar -xzf file.tar.gz -C $testDir
+cd $testDir
+wget -S -T 10 -t 5 http://elasticmapreduce.s3.amazonaws.com/bootstrap-actions/file.tar.gz
+tar -xzf file.tar.gz -C $testDir
